1d50b72af3eb0b38dcafa8fbe1d672c9afc2a5a6
liuzehui <liuzehui@mgtv.com>
2024-06-24 14:46:49 +0800

    [frei0r]modify portfile.cmake

--- ports/frei0r/portfile.cmake
+++ ports/frei0r/portfile.cmake
@@ -1,8 +1,4 @@
-include(vcpkg_common_functions)
-
-vcpkg_fail_port_install(ON_TARGET "UWP")
-
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Windows")
+if(VCPKG_TARGET_IS_WINDOWS)
     set(FREI0R_OS "win64")
     set(FREI0R_EXT "zip")
     set(FREI0R_HASH "da06ac6896c2085c33e3ffab05e836d3e2f685d859324e9feda7fe4140d645a835e8de45cc2bbf2310ce185961cb404c3af4c2ea5e57d8570013f9c9da9e2d8e")
@@ -33,6 +29,8 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE "${ARCHIVE}"
 )
 
+set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
+
 # Assuming the structure of the archive matches the expected layout
 file(GLOB H_FILES "${SOURCE_PATH}/*.h" "${SOURCE_PATH}/*.hpp")
 file(COPY ${H_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
@@ -41,6 +39,7 @@ file(COPY ${SOURCE_PATH}/filter/ DESTINATION ${CURRENT_PACKAGES_DIR}/bin/frei0r/
 file(COPY ${SOURCE_PATH}/generator/ DESTINATION ${CURRENT_PACKAGES_DIR}/bin/frei0r/generator)
 file(COPY ${SOURCE_PATH}/mixer2/ DESTINATION ${CURRENT_PACKAGES_DIR}/bin/frei0r/mixer2)
 file(COPY ${SOURCE_PATH}/mixer3/ DESTINATION ${CURRENT_PACKAGES_DIR}/bin/frei0r/mixer3)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
 
-file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/frei0r/copyright)  
-vcpkg_copy_pdbs()
+#file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/frei0r/copyright)  
+#vcpkg_copy_pdbs()
